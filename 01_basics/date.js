cc13dc15a7a675d50a460165d15aa3f91a359f68
bob-ur <140067436+bob-ur@users.noreply.github.com>
2024-04-19 13:50:16 +0000

    date

---
+++ 01_basics/date.js
@@ -0,0 +1,7 @@
+let mydate=new Date()
+console.log(mydate.toString())
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleTimeString());
